34466c5ca701c863acf65aee77a126f6d812c7c1
Polynomeer <polynomeer@gmail.com>
2021-05-13 23:12:02 +0900

    feat: Add StoreController::deleteStore (#3)
    Added StoreController::deleteStore to control `DELETE /stores/{id}`.
    Tested with printing sample string.

--- thepirates/src/main/java/com/tpirates/thepirates/controller/StoreController.java
+++ thepirates/src/main/java/com/tpirates/thepirates/controller/StoreController.java
@@ -24,7 +24,6 @@ public class StoreController {
         return storeDtoList;
     }
 
-
     @GetMapping("/{id}")
     public StoreDetailDto getStore(@PathVariable Long id) {
         List<BusinessDayDto> businessDayList = new ArrayList<>();
@@ -45,5 +44,10 @@ public class StoreController {
     @PostMapping("/{id}")
     public void addHoliday(@PathVariable Long id, @RequestBody HolidayRequestDto holidayRequestDto){
         System.out.println(holidayRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStore(@PathVariable Long id) {
+        System.out.println("Deleted store " + id);
     }
 }
