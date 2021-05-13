693ba756059670afabff5c3e532daec1acf8d689
Polynomeer <polynomeer@gmail.com>
2021-05-13 20:54:29 +0900

    build: Add H2 settings to application.properties (#1)
    Added settings about H2 Database and logging to application.properties.

--- thepirates/src/main/java/com/tpirates/thepirates/ThepiratesApplication.java
+++ thepirates/src/main/java/com/tpirates/thepirates/ThepiratesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThepiratesApplication {
+public class ThePiratesApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ThepiratesApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ThePiratesApplication.class, args);
+	}
 
 }
